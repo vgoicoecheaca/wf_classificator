db9d3d835851a31617b406b5c300991fc8226a3d
vgoicoecheaca <vgoicoecheaca@gmail.com>
2022-09-13 14:25:46 -1000

    hp config changes

--- model.py
+++ model.py
@@ -53,23 +53,30 @@ class ModelWF(Model):
 
         return pool
 
-    def HyperModel(self,units1,units2,units3,activation):
+    def HyperModel(self,units1,units2,units3,act_reg,act_hidden,l2):
         input                 = Input(shape=(self.sequence_size,1),name="input")
-        base                  = self.base(input)
+        base                  = self.HyperBase(input,l2,act_hidden)
         flatten               = Flatten()(base)
-        dense                 = Dense(units=units1,activation=self.activation)(flatten)
-        dense_class           = Dense(units=units2,activation=self.activation)(dense)
-        dense_reg             = Dense(units=units3,activation=self.activation)(dense)  
+        dense                 = Dense(units=units1,activation=act_hidden)(flatten)
+        dense_class           = Dense(units=units2,activation=act_hidden)(dense)
+        dense_reg             = Dense(units=units3,activation=act_hidden)(dense)  
         out_1                 = Dense(self.n_classes+1,activation=self.act_class,name='class')(dense_class)
-        out_2                 = Dense(1,activation,name='reg')(dense_reg)
+        out_2                 = Dense(1,act_reg,name='reg')(dense_reg)
                                                                                                             
         return Model(input,outputs=[out_1,out_2])
 
-
-
-
-
-
-
-
-
+    def HyperBase(self,x,l2_reg,activation):
+        conv                 = Conv1D(filters=2,kernel_size=1,name="conv0",activation=activation)(x)
+        pool                 = MaxPool1D(2)(conv)     
+        conv                 = Conv1D(filters=4,kernel_size=2,name="conv2",kernel_regularizer=l2(l2_reg),activation=activation)(pool)
+        pool                 = MaxPool1D(2)(conv)     
+        conv                 = Conv1D(filters=8,kernel_size=2,name="conv3",kernel_regularizer=l2(l2_reg),activation=activation)(pool)
+        pool                 = MaxPool1D(4)(conv)     
+        conv                 = Conv1D(filters=16,kernel_size=2,name="conv4",kernel_regularizer=l2(l2_reg),activation=activation)(pool)
+        pool                 = MaxPool1D(4)(conv)     
+        conv                 = Conv1D(filters=32,kernel_size=2,name="conv5",kernel_regularizer=l2(l2_reg),activation=activation)(pool)
+        pool                 = MaxPool1D(4)(conv)
+        conv                 = Conv1D(filters=64,kernel_size=2,name="conv6",kernel_regularizer=l2(l2_reg),activation=activation)(pool)
+        pool                 = MaxPool1D(4)(conv)
+                                                                                                                                             
+        return pool
