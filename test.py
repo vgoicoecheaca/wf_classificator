894db7fd40d8941abe4aca18cffd63c355b21e2e
vgoicoecheaca <vgoicoecheaca@gmail.com>
2022-09-16 10:26:10 -1000

    migration from keras.sequence, loading data on disk

--- test.py
+++ test.py
@@ -35,83 +35,68 @@ data_path           = "wfs/"
 test_sample         = [data_path+i for i in config("testing","data","str").split(",")]
 test_data_generator = DataHandler(test_sample,config)
 
+x_test, [y_true_class,y_true_reg] = test_data_generator()
+
 model = ModelWF(config) 
 model = model(True)
 model.load_weights("models/"+config("testing","model","str"))
 
-# so many loops, should find a way to optimize this 
-tpr, fpr              = np.zeros((n_batches,n_classes,n_thresholds),dtype=np.float32), np.zeros((n_batches,n_classes,n_thresholds),dtype=np.float32)
+tpr, fpr              = np.zeros((n_classes,n_thresholds),dtype=np.float32), np.zeros((n_classes,n_thresholds),dtype=np.float32)
 sample_diff           = np.zeros((n_classes-1),dtype=object)
-pars                  = np.zeros((n_batches,batch_size,len(pars_keys)),dtype=float)
-class_pred            = np.zeros((n_batches,batch_size),dtype=int)
-class_true            = np.zeros((n_batches,batch_size),dtype=int)
 hit_closeby           = []
 true_class_instances  = np.zeros((n_classes))
 pred_class_instances  = np.zeros((n_classes))
 
-#since predictions are evaluated in batches
-for batch in range(n_batches):
-    x, [y_class,y_reg]   = test_data_generator[batch]
-    locs                 = test_data_generator.get_locs()  #these are the true hit locations in the waveform
-    pars[batch]          = test_data_generator.get_pars()  #these are the parameters of the waveform, vov, gate, etc. 
-    pred_class, pred_reg = model.predict(x) 
+y_pred_class, y_pred_reg = model.predict(x_test)
+true_locs                = test_data_generator.get_locs()
+pars                     = test_data_generator.get_pars()
+pred_class_idx           = np.argmax(y_pred_class,axis=1) 
+true_class_idx           = np.argmax(y_true_class,axis=1)  
 
-    pred_class_idx       = np.argmax(pred_class,axis=1) 
-    true_class_idx       = np.argmax(y_class,axis=1)  
-    class_pred[batch]    = pred_class_idx
-    class_true[batch]    = true_class_idx 
+#closeby hit, for now only with n=2
+n2 = true_class_idx == 2
+hit_closeby.append([true_locs[n2], np.asarray(pred_class_idx[n2] == true_class_idx[n2]).astype(int)])
 
-    #closeby hit, for now only with n=2
-    n2 = true_class_idx == 2
-    hit_closeby.append([locs[n2], np.asarray(pred_class_idx[n2] == true_class_idx[n2]).astype(int) ] )
+#accuracy independent of the threshold
+v, c = np.unique(true_class_idx,return_counts=True)  
+true_class_instances[v] +=  c 
+v, c = np.unique(pred_class_idx[pred_class_idx == true_class_idx],return_counts=True)
+pred_class_instances[v] += c 
 
-    #accuracy independent of the threshold
-    v, c = np.unique(true_class_idx,return_counts=True)  
-    true_class_instances[v] +=  c 
-    v, c = np.unique(pred_class_idx[pred_class_idx == true_class_idx],return_counts=True)
-    pred_class_instances[v] += c 
-         
-    #ROC is true positive rate vs false positive rate
-    # recall is true positive / true positive + false negatives
-    thresholds = np.linspace(0,1,n_thresholds)
-    for i,threshold in enumerate(thresholds):
-        for cl in range(1,n_classes):
-            # regression performance is simply the difference between the predicted and truth beggining of pulse
-            if i==0: # otherwise it'll be repeated for each threshold.... 
-                cl_mask = true_class_idx  == cl 
-                sample_diff[cl-1] = np.append(sample_diff[cl-1], pred_reg.reshape(len(pred_reg))[cl_mask] - y_reg.numpy()[cl_mask])        
+#ROC is true positive rate vs false positive rate
+# recall is true positive / true positive + false negatives
+thresholds = np.linspace(0,1,n_thresholds)
+for i,threshold in enumerate(thresholds):
+    for cl in range(1,n_classes):
+        # regression performance is simply the difference between the predicted and truth beggining of pulse                              
+        if i==0: # otherwise it'll be repeated for each threshold.... 
+            cl_mask = true_class_idx  == cl 
+            sample_diff[cl-1] = np.append(sample_diff[cl-1],y_pred_reg[cl_mask] - y_true_reg[cl_mask])        
 
-            if cl not in true_class_idx: continue
-            over_threshold = np.zeros(true_class_idx.shape)                                                                                                           
+        if cl not in true_class_idx: continue
+        over_threshold = np.zeros(true_class_idx.shape)                                                                                                           
 
-            # if a prediciton isn't above the threshold, mark as negative (bg), if it is, mark as true (hit)
-            over_threshold[pred_class[:,cl]<=threshold] = 0 
-            over_threshold[pred_class[:,cl]>threshold]  = 1
+        # if a prediciton isn't above the threshold, mark as negative (bg), if it is, mark as true (hit)
+        over_threshold[y_pred_class[:,cl]<=threshold] = 0 
+        over_threshold[y_pred_class[:,cl]>threshold]  = 1
+ 
+        # for multiclass, negative means anythin but cl 
+        tp = len(np.where((over_threshold==1) & (true_class_idx==cl) )[0])     # pred_class isn't cl, it wasn't           -> true positive 
+        fp = len(np.where((over_threshold==1) & (true_class_idx!=cl) )[0])     # pred_class isn't cl, was                 -> false positive 
+        tn = len(np.where((over_threshold==0) & (true_class_idx!=cl) )[0])     # both pred and true are cl                -> true negative
+        fn = len(np.where((over_threshold==0) & (true_class_idx==cl) )[0])     # pred is cl but true isn't                -> false negative
 
-            # for multiclass, negative means anythin but cl 
-            tp = len(np.where((over_threshold==1) & (true_class_idx==cl) )[0])     # pred_class isn't cl, it wasn't           -> true positive 
-            fp = len(np.where((over_threshold==1) & (true_class_idx!=cl) )[0])     # pred_class isn't cl, was                 -> false positive 
-            tn = len(np.where((over_threshold==0) & (true_class_idx!=cl) )[0])     # both pred and true are cl                -> true negative
-            fn = len(np.where((over_threshold==0) & (true_class_idx==cl) )[0])     # pred is cl but true isn't                -> false negative
+        assert tp + fp + tn + fn == len(y_pred_class)
 
-            assert tp + fp + tn + fn == batch_size, "total doesn't add up"
-            tpr[batch,cl,i] = (tp / (tp+fn)) 
-            fpr[batch,cl,i] = (fp / (fp+tn)) 
+        tpr[cl,i] = (tp / (tp+fn)) 
+        fpr[cl,i] = (fp / (fp+tn)) 
 
-fpr  = np.average(fpr,axis=0)
-tpr  = np.average(tpr,axis=0)
-pars = pars.reshape(int(pars.shape[0]*pars.shape[1]),len(pars_keys))
-class_true = class_true.reshape(int(class_true.shape[0]*class_true.shape[1]))
-class_pred = class_pred.reshape(int(class_pred.shape[0]*class_pred.shape[1]))
-print(class_pred)
-print(class_true)
-
-# Plotting 
+#Plotting 
 plotter = Plotter(config)
-plotter.roc(fpr,tpr)
-plotter.acs(pred_class_instances,true_class_instances)
-plotter.regs(sample_diff)
-plotter.fake(class_true,class_pred,pars)
-plotter.single_hit_eff(class_true,class_pred,pars)
+#plotter.roc(fpr,tpr)
+#plotter.acs(pred_class_instances,true_class_instances)
+#plotter.regs(sample_diff)
+plotter.fake(true_class_idx,pred_class_idx,pars)
+plotter.single_hit_eff(true_class_idx,pred_class_idx,pars)
 plotter.double_separation(hit_closeby)
 
